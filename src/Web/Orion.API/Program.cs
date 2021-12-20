391e17125178c4ae4f97d982f9fa429b26570707
Bhushan Mulmule <bhushan.mulmule@gmail.com>
2021-12-20 14:49:05 +0000

    Merged PR 5: Changes related to .NET6.0 upgrade

--- src/Web/Orion.API/Program.cs
+++ src/Web/Orion.API/Program.cs
@@ -1,26 +1,32 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using Orion.API.CustomMiddlewares;
+using Orion.Application;
+using Orion.CosmosRepository;
+using Orion.ThirdPartyServices;
 
-namespace Orion.API
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddApplication();
+builder.Services.AddCosmosRepository(builder.Configuration);
+builder.Services.AddThirdPartyServices(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Orion.API", Version = "v1" });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+var app = builder.Build();
+
+app.UseMiddleware<ErrorHandlerMiddleware>(app.Environment);
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Orion.API v1"));
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
