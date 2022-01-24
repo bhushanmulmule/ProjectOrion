7974a270fda557867a899312e6d7716d2eb64402
bhushan-mulmule <bhushanvmulmule@gmail.com>
2022-01-24 22:10:39 +0530

    Unit Testing - Application Layer

---
+++ src/Application/Orion.Application/StoryAppLayer/UseCases/StoryUseCases/UpdateStory/UpdateStoryCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Orion.Application.StoryAppLayer.UseCases.StoryUseCases.UpdateStory
+{
+    public class UpdateStoryCommandValidator : AbstractValidator<UpdateStoryCommand>
+    {
+        public UpdateStoryCommandValidator()
+        {
+            RuleFor(v => v.Text).NotNull().NotEmpty()
+                .MaximumLength(300);
+        }
+    }
+}
